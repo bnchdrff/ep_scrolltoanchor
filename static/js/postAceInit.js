9b050918bee8971815cfa593a30ce19fbe4b0822
Benjamin Chodoroff <ben@falafelcopter.com>
2021-02-28 15:05:38 -0500

    works with latest etherpad-lite now, bump version also

--- static/js/postAceInit.js
+++ static/js/postAceInit.js
@@ -1,41 +1,47 @@
-exports.postAceInit = function(hook, context) {
-
-  var anchor = getUrlVars()['anchor'];
+exports.postAceInit = function (hook, context) {
+  var anchor = getUrlVars()["anchor"];
   if (anchor) {
-    findAnchorAndScrollTo(anchor);
+    findAnchorAndScrollTo('#' + anchor.trim());
   }
-
 };
 
 function findAnchorAndScrollTo(anchor) {
-  var count = 1;
-  $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").contents().each(function() {
-    if ($(this).text().trim() == '#' + anchor.trim()) {
-      var newY = $(this).context.offsetTop + "px";
-      var $outerdoc = $('iframe[name="ace_outer"]').contents().find("#outerdocbody");
-      var $outerdocHTML = $('iframe[name="ace_outer"]').contents().find("#outerdocbody").parent();
-      $outerdoc.animate({scrollTop: newY});
-      if (browser.mozilla || browser.firefox) $outerdocHTML.animate({scrollTop: newY}); // needed for FF
-      return false;
+  var $outer = $('iframe[name="ace_outer"]').contents();
+
+  var lines = $outer.find("iframe").contents().find("#innerdocbody").contents();
+
+  for (var i = 0; i < lines.length; i++) {
+    var line = lines[i];
+    if (line.textContent.trim().startsWith(anchor)) {
+      var newY = line.offsetTop;
+      var $outerdoc = $outer.find("#outerdocbody");
+
+      $outerdoc.animate({ scrollTop: newY });
+      if (browser.mozilla || browser.firefox)
+        $outerdoc.parent().animate({ scrollTop: newY }); // needed for FF
+      break;
     }
-    count++;
-  });
+  }
 }
 
 function getUrlVars() {
-  var vars = [], hash;
-  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-  for(var i = 0; i < hashes.length; i++) {
-    hash = hashes[i].split('=');
+  var vars = [],
+    hash;
+  var hashes = window.location.href
+    .slice(window.location.href.indexOf("?") + 1)
+    .split("&");
+  for (var i = 0; i < hashes.length; i++) {
+    hash = hashes[i].split("=");
     vars.push(hash[0]);
     vars[hash[0]] = hash[1];
   }
-  var hashes = window.location.href.slice(window.location.href.indexOf('#') + 1).split('&');
-  for(var i = 0; i < hashes.length; i++) {
-    hash = hashes[i].split('=');
+  var hashes = window.location.href
+    .slice(window.location.href.indexOf("#") + 1)
+    .split("&");
+  for (var i = 0; i < hashes.length; i++) {
+    hash = hashes[i].split("=");
     vars.push(hash[0]);
     vars[hash[0]] = hash[1];
   }
   return vars;
 }
-
